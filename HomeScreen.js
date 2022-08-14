aae99468cd0109523706ea3168a86a9e97cbb588
Ibadullah-Kahttana <ibadullahkahttana@gmail.com>
2022-08-14 15:04:31 +0500

    Commit my Code

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,54 +1,66 @@
 import React from 'react';
 import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const HomeScreen = ({ navigation }) => {
-
-  const { colors } = useTheme();
+const HomeScreen = ({navigation}) => {
+  const {colors} = useTheme();
 
   const theme = useTheme();
 
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={'black'} barStyle={theme.dark ? "dark-content" : "light-content"} />
-
+      <StatusBar
+        backgroundColor={'black'}
+        barStyle={theme.dark ? 'dark-content' : 'light-content'}
+      />
       // Header Bar
       <View style={styles.headerBar}>
-
         <TouchableOpacity onPress={() => navigation.openDrawer()}>
           <Icon name="ios-menu" color="black" size={30} />
         </TouchableOpacity>
 
-        <Text style={{ fontSize: 20, color: 'black', fontWeight: 'bold' }}> Order Food </Text>
+        <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+          {' '}
+          Order Food{' '}
+        </Text>
 
-        <TouchableOpacity onPress={()=>{navigation.navigate('Profile')}}>
-        <View style={{
-          width: (windowWidth / 100) * 12,
-          height: (windowHeight / 100) * 6,
-          borderRadius:50,
-          alignItems:'center',
-          justifyContent:'center'
-        }}><Image
-            source={require('./assets/assanDukanPict/logo.png')}
-            style={styles.logo}
-            resizeMode="stretch"
-          /></View>
-
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Profile');
+          }}>
+          <View
+            style={{
+              width: (windowWidth / 100) * 12,
+              height: (windowHeight / 100) * 6,
+              borderRadius: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image
+              source={require('./assets/assanDukanPict/logo.png')}
+              style={styles.logo}
+              resizeMode="stretch"
+            />
+          </View>
         </TouchableOpacity>
       </View>
-<<<<<<< HEAD
-<Text>commit from majeed </Text>
-=======
-
-<View>
-  <Text> Commit From Ibad</Text>
-</View>
->>>>>>> bff66ae7eeb3a9f6a60461e1081ffd91d53f2f3d
-
+      <View>
+        <Text> Commit From Ibad</Text>
+      </View>
     </View>
   );
 };
@@ -67,14 +79,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingLeft: 15,
     flexDirection: 'row',
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     alignItems: 'center',
-    paddingRight: 15
+    paddingRight: 15,
   },
   logo: {
     width: (windowWidth / 100) * 12,
     height: (windowHeight / 100) * 6,
-
   },
-
 });
